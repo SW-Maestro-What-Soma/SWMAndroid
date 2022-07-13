5e203d33aa5f1ce4749d7389f9b68e40df0f4244
koreatlwls <koreatlwls@gimsijin-ui-MacBookAir.local>
2022-07-13 16:43:34 +0900

    feat : Implementation main view implementation main activity view

---
+++ app/src/main/java/com/example/swmandroid/FullServiceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.swmandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FullServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_service)
+    }
+}
--- app/src/main/java/com/example/swmandroid/MainActivity.kt
+++ app/src/main/java/com/example/swmandroid/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.swmandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.swmandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fullServiceButton.setOnClickListener { moveActivity(FullServiceActivity()) }
+
+        //Todo 닉네임 로직 구현시 변경
+        binding.nickNameTextview.text = "김시진님"
+
+        binding.myPageButton.setOnClickListener { moveActivity(MyPageActivity()) }
+        binding.easyLearningButton.setOnClickListener { moveActivity(EasyLearningActivity()) }
+        binding.testButton.setOnClickListener { moveActivity(TestActivity()) }
+        binding.communityButton.setOnClickListener { moveActivity(CommunityActivity()) }
+    }
+
+    private fun moveActivity(goToActivity: AppCompatActivity) {
+        val intent = Intent(this, goToActivity::class.java)
+        startActivity(intent)
     }
 }
