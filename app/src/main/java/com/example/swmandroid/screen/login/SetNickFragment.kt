d586e475a6ffe92506bd091b166a411a5bccc092
koreatlwls <koreatlwls@gmail.com>
2022-07-23 16:17:45 +0900

    feat : Implementation nickname fragment

--- app/src/main/java/com/example/swmandroid/screen/login/SetNickFragment.kt
+++ app/src/main/java/com/example/swmandroid/screen/login/SetNickFragment.kt
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.swmandroid.R
@@ -34,7 +36,17 @@ class SetNickFragment : Fragment() {
 
     private fun buttonClick() = with(binding) {
         backButton.setOnClickListener { root.findNavController().popBackStack() }
-        setNickButton.setOnClickListener { root.findNavController().navigate(R.id.action_setNickFragment_to_mainActivity) }
+        setNickButton.setOnClickListener {
+            //TODO 이메일 가져와서 이메일+스택+닉네임 등록 로직 추가해야함
+            if(checkNickname(setNickEdittext)){
+                val techStack = requireArguments().getString("tech_stack")
+                val nickName = setNickEdittext.text.toString().trim()
+                root.findNavController().navigate(R.id.action_setNickFragment_to_mainActivity)
+            }else {
+                Toast.makeText(context, "닉네임 형식을 확인해주세요.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
     private fun checkNickEditText(nicknameEditText: EditText) {
@@ -49,7 +61,7 @@ class SetNickFragment : Fragment() {
 
     @SuppressLint("ResourceAsColor")
     private fun checkNickname(nicknameEditText: EditText): Boolean {
-        val nickValidation = "^[가-힣ㄱ-ㅎa-zA-Z0-9._ -]{2,}\$"
+        val nickValidation = "^[가-힣ㄱ-ㅎa-zA-Z0-9]{2,}\$"
         val nick = nicknameEditText.text.toString().trim()
         val pattern = Pattern.matches(nickValidation, nick)
         return if (pattern) {
