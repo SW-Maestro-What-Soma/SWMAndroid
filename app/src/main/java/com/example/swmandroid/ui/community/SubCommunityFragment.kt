76a44f6645c300d3cbd6b8b1c29439912c878b2a
koreatlwls <koreatlwls@gmail.com>
2022-08-19 21:51:45 +0900

    feat : Publishing 전체 게시판

--- app/src/main/java/com/example/swmandroid/ui/community/SubCommunityFragment.kt
+++ app/src/main/java/com/example/swmandroid/ui/community/SubCommunityFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.swmandroid.R
 import com.example.swmandroid.base.BaseFragment
